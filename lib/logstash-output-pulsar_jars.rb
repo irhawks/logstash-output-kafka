f71767aefcb5dfd0231f4078ba129d99dff11984
root <root@node25.mach.irhawks.com>
2019-05-07 07:38:25 +0000

    update

--- lib/logstash-output-pulsar_jars.rb
+++ lib/logstash-output-pulsar_jars.rb
@@ -2,6 +2,7 @@
 
 require 'jar_dependencies'
 require_jar('org.apache.pulsar', 'pulsar-client-kafka', '2.3.1')
+require_jar('org.apache.pulsar', 'pulsar-client', '2.3.1')
 require_jar('com.github.luben', 'zstd-jni', '1.3.7-3')
 require_jar('org.slf4j', 'slf4j-api', '1.7.25')
 require_jar('org.lz4', 'lz4-java', '1.5.0')
